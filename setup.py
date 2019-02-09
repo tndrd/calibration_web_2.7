a084af1a3695cb8a722d702198c8d68e5a5bf200
tinderad <44276006+tinderad@users.noreply.github.com>
2019-02-09 19:43:04 +0300

    updated

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ from os.path import join, dirname
 setup(
     name='ccc_server',
     version='1.0',
-    entry_points={'console_scripts': ['runserver = app']})
+    packages=["ccc_server"],
+    entry_points={'console_scripts': ['runserver = ccc_server.app']})
